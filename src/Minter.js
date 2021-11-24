8a8a55116d0c650f68912a93bcdaa1ac7a325ff1
Anas Sallem <anassallem@cyberdivision.tn>
2021-11-24 15:26:56 +0100

    Initial Commit

---
+++ src/Minter.js
@@ -0,0 +1,116 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected, mintNFT } from "./utils/interact.js";
+
+const Minter = (props) => {
+
+  //State variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+ 
+  useEffect(async () => {
+    const {address, status} = await getCurrentWalletConnected();
+    setWallet(address)
+    setStatus(status); 
+
+    addWalletListener();
+  }, []);
+
+  const connectWalletPressed = async () => { 
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);  
+  };
+
+  const onMintPressed = async () => {
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);
+  };
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+      <h1 id="title">🧙‍♂️ Alchemy NFT Minter</h1>
+      <p>
+        Simply add your asset's link, name, and description, then press "Mint."
+      </p>
+      <form>
+        <h2>🖼 Link to asset: </h2>
+        <input
+          type="text"
+          placeholder="e.g. https://gateway.pinata.cloud/ipfs/<hash>"
+          onChange={(event) => setURL(event.target.value)}
+        />
+        <h2>🤔 Name: </h2>
+        <input
+          type="text"
+          placeholder="e.g. My first NFT!"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <h2>✍️ Description: </h2>
+        <input
+          type="text"
+          placeholder="e.g. Even cooler than cryptokitties ;)"
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+        Mint NFT
+      </button>
+      <p id="status">
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Minter;
+
+ //API Key: d93c922395e288102709
+ //API Secret: 43c6813b09e7957ce2ac89a423ce4c874314e991daffcaee96c97527a94a53ad
+ //JWT: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIwN2E3ZmY2My1lNmExLTRhNzItYjAwNy00ODNjZWI5ZGJhZDQiLCJlbWFpbCI6ImFuYXMuc2FsbGVtNzNAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZX0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6ImQ5M2M5MjIzOTVlMjg4MTAyNzA5Iiwic2NvcGVkS2V5U2VjcmV0IjoiNDNjNjgxM2IwOWU3OTU3Y2UyYWM4OWE0MjNjZTRjODc0MzE0ZTk5MWRhZmZjYWVlOTZjOTc1MjdhOTRhNTNhZCIsImlhdCI6MTYzNzc1ODE4MX0.LU04Z-uW7QlLXQkNF0cV8h-gEOq_uhPo5gqovO8Mw3U
+
+
+ 
